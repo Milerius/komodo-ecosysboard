493a52b363633879068526bbcaaa9c3e4dffa3b0
milerius <rmscastle@gmail.com>
2019-07-09 19:51:43 +0200

    test: add log to the unit tests

--- ecosysboard/http/dexstats_test.go
+++ ecosysboard/http/dexstats_test.go
@@ -27,6 +27,9 @@ func TestAddressDetailsDexstats(t *testing.T) {
 	cfg := &config.Config{HTTPPort: 8081}
 	go LaunchServer(cfg)
 	statusCode, body, err := fasthttp.Get(nil, "http://localhost:8081/api/v1/dexstats/addr/kmd/RSp8vhyL6hN3yqn5V1qje62pBgBE9fv3Eh")
+	if err != nil {
+		t.Logf("err: %v", err)
+	}
 	assert.EqualValuesf(t, 200, statusCode, "status code should be 200")
 	assert.Nilf(t, err, "err should be nil")
 	assert.NotEmptyf(t, body, "body should not be empty")
@@ -36,6 +39,9 @@ func TestGetTransactionDetailsDexstats(t *testing.T) {
 	cfg := &config.Config{HTTPPort: 8082}
 	go LaunchServer(cfg)
 	statusCode, body, err := fasthttp.Get(nil, "http://localhost:8082/api/v1/dexstats/tx/kmd/11ef4a504b4b5573bf9311c9f84e263f5535ec8a671e79d746769bda4b83fcb1")
+	if err != nil {
+		t.Logf("err: %v", err)
+	}
 	assert.EqualValuesf(t, 200, statusCode, "status code should be 200")
 	assert.Nilf(t, err, "err should be nil")
 	assert.NotEmptyf(t, body, "body should not be empty")
@@ -45,6 +51,9 @@ func TestUTXODetailsDexstats(t *testing.T) {
 	cfg := &config.Config{HTTPPort: 8083}
 	go LaunchServer(cfg)
 	statusCode, body, err := fasthttp.Get(nil, "http://localhost:8083/api/v1/dexstats/addrs/kmd/RSXGTHQSqwcMw1vowKfEE7sQ8fAmv1tmso/utxo")
+	if err != nil {
+		t.Logf("err: %v", err)
+	}
 	assert.EqualValuesf(t, 200, statusCode, "status code should be 200")
 	assert.Nilf(t, err, "err should be nil")
 	assert.NotEmptyf(t, body, "body should not be empty")
