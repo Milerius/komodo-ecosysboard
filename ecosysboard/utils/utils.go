4e09f5a59f18c84e9bde4d9d3df2ae7cc673d079
Sztergbaum Roman <rmscastle@gmail.com>
2019-07-12 11:02:23 +0200

    feat(utils): add kmd address validator (#9)

---
+++ ecosysboard/utils/utils.go
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Copyright © 2013-2019 The Komodo Platform Developers.                      *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Komodo Platform software, including this file may be copied, modified,     *
+ * propagated or distributed except according to the terms contained in the   *
+ * LICENSE file                                                               *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+package utils
+
+import "regexp"
+
+func IsKomodoAddress(address string) bool {
+	var re = regexp.MustCompile(`(?m)^[R][a-km-zA-HJ-NP-Z1-9]{27,34}$`)
+	return re.MatchString(address)
+}
